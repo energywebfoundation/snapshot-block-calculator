9fc2875bc00202bd6bce64884395431f20886fc7
John Henderson <john.henderson@energyweb.org>
2022-07-05 12:37:10 +0200

    feat: using realistic block numbers

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
 fn main() {
+    let start_block = 17690000;
+    let end_block = 18200000;
     let mut rng = ChaCha8Rng::seed_from_u64(2);
-    println!("{}", rng.gen_range(0..100));
+    println!("{}", rng.gen_range(start_block..end_block));
 }
