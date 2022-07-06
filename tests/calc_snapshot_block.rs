804ad8ef0bb8b3c3d75388448a60a6162ed73eb2
John Henderson <john.henderson@energyweb.org>
2022-07-06 15:21:30 +0200

    test: add test for reproducibility

--- tests/calc_snapshot_block.rs
+++ tests/calc_snapshot_block.rs
@@ -15,6 +15,17 @@ mod calc_snapshot_block {
     }
 
     #[test]
+    fn test_output_is_reproducible() {
+      let start_block: u32 = 17690000; // https://explorer.energyweb.org/block/17690000/transactions
+      let end_block:u32 = 18200000; // https://explorer.energyweb.org/block/18200000/transactions
+      let block_hash = "0xfdd6d56dc922bf093cd69abb72f4b1d33d1a4a9cd7978a04c59f97ad0345bada"; // https://explorer.energyweb.org/block/18778013/transactions
+      let output_1 = calc_snapshot_block(start_block, end_block, block_hash);
+      let output_2 = calc_snapshot_block(start_block, end_block, block_hash);
+      assert_eq!(output_1, output_2);
+    }
+
+
+    #[test]
     fn test_can_generate_different_outputs() {
       let start_block: u32 = 17690000; // https://explorer.energyweb.org/block/17690000/transactions
       let end_block:u32 = 18200000; // https://explorer.energyweb.org/block/18200000/transactions
