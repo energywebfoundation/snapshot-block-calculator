915b630a23c9b341db2c166ca3832ee0bc2ffe52
John Henderson <jrhender@users.noreply.github.com>
2022-09-12 01:58:50 -0600

    chore: add consortia snapshot3 calc (#4)

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use snapshot_block_calculator::calc_snapshot_block;
 /// Executes calc_snapshot_block.
 /// Change values as necessary.
 fn main() {
-  let start_block: u32 = 18197776; // https://explorer.energyweb.org/block/17675146/transactions
-  let end_block:u32 = 19142937; // https://explorer.energyweb.org/block/18197775/transactions
-  let block_hash = "0x2d47301fe8af4a1d4d94f1868930eedf216ce995090ee8534c8fa5cbf2f291f3"; // https://explorer.energyweb.org/block/19153600/transactions
+  let start_block: u32 = 19142938; // https://explorer.energyweb.org/block/19142938/transactions
+  let end_block:u32 = 19742310; // https://explorer.energyweb.org/block/19742310/transactions
+  let block_hash = "0xb424122b9379b1dff91dc5d15e2b25b0a55a90c4fe9c61e27f3181f6cc744ca3"; // https://explorer.energyweb.org/block/19857275/transactions
   println!("{}", calc_snapshot_block(start_block, end_block, block_hash));
 }
 
--- tests/calc_snapshot_block.rs
+++ tests/calc_snapshot_block.rs
@@ -83,4 +83,15 @@ mod calc_snapshot_block {
       assert!(output < end_block);
     }
 
+    #[test]
+    fn test_consortia_snapshot3() {
+      let start_block: u32 = 19142938; // https://explorer.energyweb.org/block/19142938/transactions
+      let end_block:u32 = 19742310; // https://explorer.energyweb.org/block/19742310/transactions
+      let block_hash = "0xb424122b9379b1dff91dc5d15e2b25b0a55a90c4fe9c61e27f3181f6cc744ca3"; // https://explorer.energyweb.org/block/19857275/transactions
+      let output = calc_snapshot_block(start_block, end_block, block_hash);
+      assert_eq!(output, 19190154); // https://explorer.energyweb.org/block/18059849/transactions
+      assert!(output > start_block);
+      assert!(output < end_block);
+    }
+
 }
